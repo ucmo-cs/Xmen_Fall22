7e595cc8eda4fb69adeb35570ec9a9a788838758
Samuel Luke <samluke96@gmail.com>
2022-10-15 15:18:53 -0500

    Filled out location page
    Still need to add functionality to buttons and searchbar. I just put an image of the map

--- React/src/Components/SearchBox.js
+++ React/src/Components/SearchBox.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBox = (props) => {
     return(
         <input type='search'
-         className='search'
+         className='search-bar'
          placeholder={props.placeholder}
          onChange={props.handleChange} />
     )
--- React/src/Pages/Location.js
+++ React/src/Pages/Location.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import SearchBox from "../Components/SearchBox";
+import Button from "react-bootstrap/Button";
+import map from "../Photos/Picture2.png";
+import Card from 'react-bootstrap/Card';
 
 function Location(){
 
@@ -7,9 +10,59 @@ function Location(){
 
     return (
         <div className="location">
-            <h2>Please pick one of our locations</h2>
-            <SearchBox placeholder="Search Location" handleChange={(e) => setSearch({searchField:e.target.value})}/>
+            <h2 className="location-header">Please pick one of our locations</h2>
+            <div className="location-search">
+                <SearchBox placeholder="Search Location" handleChange={(e) => setSearch({searchField:e.target.value})}/>
+                <Button className="search-button" variant="primary">Search</Button>
+            </div>
+            <img className="location-map" src={map} alt="map"/>
+            <div className="location-pick">
+                <Card
+                    bg="light"
+                    border="success"
+                    style={{ width: '13.5rem', height:"250px"}}
+                    className="mb-2"
+                >
+                    <Card.Body>
+                        <Card.Title> Summer Set Dr Warrensburg, MO 64093 </Card.Title>
+                        <Card.Text>
+                            <p> {' '} </p>
+
+                            <small className="appointments-text"> available appointments next week </small>
+
+                        </Card.Text>
+                        <Card.Footer>
+                            <p> {' '} </p>
+                            <Button className="button-select-location" variant="primary">Select Location</Button>
+                        </Card.Footer>
+
+                    </Card.Body>
+                </Card>
+                <Card
+                    bg="light"
+                    border="success"
+                    style={{ width: '13.5rem', height:"250px", marginLeft:"20px"}}
+                    className="mb-2"
+                >
+                    <Card.Body>
+                        <Card.Title> 505 Tractors pass Centerview, MO 64093 </Card.Title>
+                        <Card.Text>
+                            <p> {' '} </p>
+
+                            <small className="appointments-text"> available appointments next week </small>
+
+                        </Card.Text>
+                        <Card.Footer>
+                            <p> {' '} </p>
+                            <Button className="button-select-location" variant="primary">Select Location</Button>
+                        </Card.Footer>
+
+                    </Card.Body>
+                </Card>
+
+            </div>
         </div>
+
     );
 }
 
